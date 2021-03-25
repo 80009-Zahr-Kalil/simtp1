430ff1ca8a666588afa5d2b625880441aefba901
kalil20 <kalilzahr20@gmail.com>
2021-03-25 10:22:37 -0300

    version para fd

--- script.js
+++ script.js
@@ -24,10 +24,122 @@ function metodo() {
     return nombreClase;
 }
 
-/*
-CODIGO INEFICIENTE -> 1M registros = 17 segundos
-*/
+var constantes = [];
+var x0Actual;
 
+function obtenerValores() {
+    const start = Date.now();
+    var arr;
+    var nombreClase = metodo();
+    var datos = document.getElementsByClassName(nombreClase);
+    var n = Number(datos[0].value);
+    var x0 = Number(datos[1].value);
+    var k = Number(datos[2].value);
+    var g = Number(datos[3].value);
+    var m = Number(datos[4].value);
+    var a = Number(datos[5].value);
+    var c;
+    if(datos[6] != null) {
+        c = Number(datos[6].value);
+    } else {
+        c = 0;
+    }
+    arr = [n, x0, k, g, m, a, c];
+    //console.log(arr);
+    registrosActuales = n;
+    x0Actual = x0;
+    console.log("OBTENER VALORES -> " + (Date.now()-start));
+    return arr;
+}
+
+function calcular(arr) {
+    var nombreClase = metodo();
+    if(nombreClase == "completar2") {
+        arr[6] = 0;
+    }
+    var n1 = arr[5] * x0Actual + arr[6];
+    
+    // calculo Xi+1
+    var n2 = n1 % arr[4];
+
+    // calculo el valor aleatorio
+    var rnd = n2 / (arr[4]-1);
+    var redondeado = Number(rnd.toFixed(4));
+    //console.log(redondeado);
+    //arr[1] = n2;
+    //$('.' + nombreClase)[1].value = n2;
+    x0Actual = n2;
+    return redondeado;
+}
+
+
+function primerosRegistros() {
+    const start = Date.now()
+    constantes = obtenerValores();
+    var cantidadRegistros = constantes[0];
+    var listaNumeros = [];
+    for(var i=0; i<cantidadRegistros; i++) {
+        var numero = calcular(constantes);
+        listaNumeros.push(numero);
+    }
+    console.log("PRIMEROS REGISTROS -> " + (Date.now()-start));
+    return listaNumeros;
+}
+var registrosActuales;
+var element = document.getElementsByClassName("rellenar")[0];
+
+function rellenarCadena(i, numero) {
+    var cadena = '<p>' + i  + ' ' + numero + '</p>';
+    element.insertAdjacentHTML('beforeend', cadena);
+}
+
+
+//var cadena;
+
+
+function mostrar() {
+    var arr = primerosRegistros();
+    var cadena = "";
+    const start = Date.now();
+    for(var i=0; i<arr.length; i++) {
+        //rellenarCadena(i, arr[i]);
+        cadena += '<p>' + i  + ' ' + arr[i] + '</p>';
+    }
+    //element.insertAdjacentHTML('beforeend', cadena);
+    document.getElementsByClassName("output")[0].style.display = "block";
+    $(".rellenar").html(cadena);
+    $(".output").show();
+    console.log("MOSTRAR -> " + (Date.now()-start));
+}
+
+function agregarRegistro() {
+    registrosActuales++;
+    var nuevoNumeroRandom = calcular(constantes);
+    var cadena = '<p>' + (registrosActuales-1)  + ' ' + nuevoNumeroRandom + '</p>';
+    document.getElementsByClassName("rellenar")[0].insertAdjacentHTML('beforeend', cadena);
+    $(".output").show();
+}
+
+/*
+function agregarRegistro(cadena) {
+    registrosActuales++;
+    console.log("RA---->" + registrosActuales);
+    //var nuevosValores = obtenerValores();
+    var nuevoNumeroRandom = calcular(constantes);
+    cadena += '<p>' + nuevoNumeroRandom + '</p>';
+    //$(".rellenar").html() += '<p>' + nuevoNumeroRandom + '</p>';
+    //cadena += '<p>' + nuevoNumeroRandom + '</p>'
+    $(".rellenar").html(cadena);
+    $(".output").show();
+    //mostrar();
+    //var cadenaPrimerosRegistros = mostrar();
+    //cadenaPrimerosRegistros += '<p>' + ' ' + calcular(nuevosValores) + '</p>'
+    //$(".rellenar").html(cadenaPrimerosRegistros);
+}
+*/
+//CODIGO INEFICIENTE -> 1M registros = 17 segundos
+
+/*
 
 // calcular los numeros aleatorios
 function calcular(className) {
@@ -87,6 +199,10 @@ function agregarRegistro() {
     calcular(nombreClase);
     mostrar();
 }
+*/
+
+
+
 
 
 /*
@@ -103,8 +219,5 @@ function calcular(x0, k, g, m, a, c) {
     x0 = n2;
 }
 
-calcular(6,3,3,8,13,7);*/
-
-/*
-Necesito tener dos funciones para calcular, la p
+//calcular(6,3,3,8,13,7);
 */
